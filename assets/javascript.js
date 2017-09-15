baa634d55468c89ecaf60064803b4a082f9f5c01
Daniel Cherrez <Dani4543@Pablyni-2.local>
2017-09-15 15:18:21 -0400

    Initial commit

--- assets/javascript.js
+++ assets/javascript.js
@@ -15,16 +15,19 @@ else if(navigator.getUserMedia) { // Standard
     navigator.getUserMedia({ video: true }, function(stream) {
         video.src = stream;
         video.play();
+        console.log("Stream1: " + stream);
     }, errBack);
 } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
     navigator.webkitGetUserMedia({ video: true }, function(stream){
         video.src = window.webkitURL.createObjectURL(stream);
         video.play();
+        console.log("Stream2: " + stream);
     }, errBack);
 } else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
     navigator.mozGetUserMedia({ video: true }, function(stream){
         video.src = window.URL.createObjectURL(stream);
         video.play();
+        console.log("Stream3: " + stream);
     }, errBack);
 }
 
